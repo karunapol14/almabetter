a7b946ccb9ea49108ba00d47f9cdd83de756a67d
almabetter <karunapol2004@gmail.com>
2024-03-10 12:21:14 +0530

    var

---
+++ 1st.js
@@ -0,0 +1,16 @@
+//variables=Variable is a name given to a memory location that acts as a container for storing data temporarily. 
+//They are nothing but reserved memory locations to store values.
+
+
+
+var a = "John Doe";
+
+var someBool = true;
+
+if (someBool) {
+
+var a = "Daniel Joan";
+
+}
+
+console.log(a);
